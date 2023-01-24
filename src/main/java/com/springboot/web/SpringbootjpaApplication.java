80403987ec94e4c9af373ac8702d921d56537dd8
roboanonymous <jsamyak591@gmail.com>
2023-01-24 18:06:56 +0530

    first

--- src/main/java/com/springboot/web/SpringbootjpaApplication.java
+++ src/main/java/com/springboot/web/SpringbootjpaApplication.java
@@ -2,12 +2,27 @@ package com.springboot.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.springboot.Entity.User;
+import com.springboot.dao.UserRepository;
 
 @SpringBootApplication
 public class SpringbootjpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootjpaApplication.class, args);
+		
+		ApplicationContext context = SpringApplication.run(SpringbootjpaApplication.class, args);
+		
+		UserRepository UserRepository = context.getBean(UserRepository.class);
+		User user = new User();
+		user.setName("Samyak");
+		user.setCity("Hansi");
+		user.setStatus("I am Java Developer");
+		
+		User user1 = UserRepository.save(user);
+		System.out.println(user1);
 	}
+	
 
 }
